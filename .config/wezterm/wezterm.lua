db3c18a24dd75766c2a10eafe82773a1d55cc2b4
takumi0706 <ganndamu0706@gmail.com>
2026-01-01 15:41:46 +0900

    feat: change font size

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local config = wezterm.config_builder()
 
 config.automatically_reload_config = true
 config.font = wezterm.font("JetBrainsMono Nerd Font")
-config.font_size = 12.0
+config.font_size = 12.5
 config.use_ime = true
 config.window_background_opacity = 0.75
 config.macos_window_background_blur = 20
